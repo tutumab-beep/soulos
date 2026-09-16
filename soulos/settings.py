58258c7a167ae0bdbe19e1cce25323989f161634
Tutu Authellow Mabunda <tutu.mabu@gmail.com>
2026-09-16 18:46:33 +0200

    Update healing-styles.css and fix STATIC_ROOT

--- soulos/settings.py
+++ soulos/settings.py
@@ -128,14 +128,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
 
 
 LOGIN_REDIRECT_URL = "dashboard_app:home"
